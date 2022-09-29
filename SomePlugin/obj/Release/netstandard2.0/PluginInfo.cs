af149bc0341c31d1dff346f04019242b92903359
Aragami <tim.velling@hotmail.de>
2022-09-29 21:27:58 +0200

    Init

---
+++ SomePlugin/obj/Release/netstandard2.0/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace SomePlugin
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "SomePlugin";
+        public const string PLUGIN_NAME = "SomePlugin";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
