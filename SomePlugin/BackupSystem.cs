b8963a2d002aa747aaa178bd9079e7a08489087b
Aragami-delp <58348768+Aragami-delp@users.noreply.github.com>
2022-10-05 17:03:09 +0200

    Files to folders

--- SomePlugin/BackupSystem.cs
+++ SomePlugin/BackupSystem.cs
@@ -5,37 +5,38 @@ using System.Collections.Generic;
 using System.IO;
 
 public class BackupSystem : Monobehaviour {
-    public List<SaveFile> m_saves;
+    public List<SaveFolder> m_saves;
+    private string m_gameDataPath;
 
     private void Start() {
-        LoadSaveFiles()
+        m_gameDataPath = Application.persistentDataPath.SaveFiles + "\\It's Happening\\PlateUp"
+        LoadSaveFolders()
     }
 
-    public void SelectSaveFile(SaveFile _selected) {
+    public void SelectSaveFolder(SaveFolder _selected) {
         // TODO: Load save into game
     }
 
-    public void LoadSaveFiles() {
-        foreach(string filePath in Directory.GetFiles(Application.persistentDataPath.SaveFiles)) // TODO: Find correct path
-        {
-            AddSaveFile(filePath);
-        }
+    public void LoadSaveFolders() {
+        if (Directory.Exists(m_gameDataPath + "\\Full") && Directory.GetFiles(m_gameDataPath + "\\Full").Length >= 0)
+        foreach(string filePath in Directory.GetFiles("{Application.persistentDataPath.SaveFiles}\\It's Happening\\PlateUp")) // TODO: Find correct path
+            AddSaveFolder(filePath);
         m_save.Sort();
     }
 
-    private void AddSaveFile(string _filePath) {
+    private void AddSaveFolder(string _filePath) {
         if (File.Exists(path))
         {
-            m_save.Add(new SaveFile(_filePath));
+            m_save.Add(new SaveFolder(_filePath));
         }
     }
 }
 
-public class SaveFile : IComparable<SaveFile> {
+public class SaveFolder : IComparable<SaveFolder> {
     public readonly string name;
     public readonly DateTime modifiedTime;
     
-    public override int CompareTo(SaveFile _other) {
+    public override int CompareTo(SaveFolder _other) {
         return modifiedTime.CompareTo(_other.modifiedTime);
     }
 
